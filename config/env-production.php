e494665db08e65b8f84b2b17bf8d85491af5d198
yuki ueno <yuki.weaver1204@gmail.com>
2020-11-20 17:56:05 +0900

    env.php 一旦削除

--- config/env-production.php
+++ config/env-production.php
@@ -1,10 +1,10 @@
 <?php
 
 /** DB情報 **/
-$host       = '127.0.0.1';
-$dbname     = 'miniblog';
-$user       = 'root';
-$password   = 'password';
+$host       = 'us-cdbr-east-02.cleardb.com';
+$dbname     = 'heroku_bc0473c70c4f578';
+$user       = 'bcce9fd9ac496e';
+$password   = 'aa05baa6';
 $g_dbInfo   = [
   'host'      => $host,
   'dbname'    => $dbname,
