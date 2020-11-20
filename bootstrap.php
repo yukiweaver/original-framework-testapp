cc55eb9fa7c19adaa3cc5b1fc6b32b0635bae7f1
yuki ueno <yuki.weaver1204@gmail.com>
2020-11-20 18:15:00 +0900

    開発、本番でDB接続先を変更

--- bootstrap.php
+++ bootstrap.php
@@ -2,9 +2,16 @@
 
 require dirname(__FILE__) . '/core/ClassLoader.php';
 require_once dirname(__FILE__) . '/config/message.php';
-require_once dirname(__FILE__) . '/config/env.php';
 require_once dirname(__FILE__) . '/config/route_definition.php';
 require_once dirname(__FILE__) . '/vendor/autoload.php';
+
+if (file_exists(dirname(__FILE__) . '/config/env-develop.php')) {
+  // 開発環境
+  require_once dirname(__FILE__) . '/config/env-develop.php';
+} else {
+  // 本番環境
+  require_once dirname(__FILE__) . '/config/env-production.php';
+}
 
 $loader = new ClassLoader();
 
--- web/index_dev.php
+++ web/index_dev.php
@@ -26,6 +26,11 @@
 // エラーメッセージなどメッセージはmessage.php
 // その他設定はenv.inc
 
+/** 開発環境と本番環境 **/
+// DBの接続情報はenv-developmentファイルがあるかどうかで判断する
+// そのため、gitの追跡対象にenv-develop.phpは入れない
+// また、開発でenv-develop.phpのコードを変更した場合は、同様の変更をenv-production.phpにもする
+
 /********** 開発ルール ここまで **********/
 
 
